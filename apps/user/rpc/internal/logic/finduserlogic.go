34451ab01cfc9d029c7f0c26c8e8ceeaa562ed8e
Donkor- <donkor@yeah.net>
2024-09-15 18:16:03 +0800

    搜索用户，获取用户信息，新增model文件

--- apps/user/rpc/internal/logic/finduserlogic.go
+++ apps/user/rpc/internal/logic/finduserlogic.go
@@ -2,6 +2,9 @@ package logic
 
 import (
 	"context"
+	"easy-chat/apps/user/models"
+	"fmt"
+	"github.com/jinzhu/copier"
 
 	"easy-chat/apps/user/rpc/internal/svc"
 	"easy-chat/apps/user/rpc/user"
@@ -24,7 +27,30 @@ func NewFindUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *FindUser
 }
 
 func (l *FindUserLogic) FindUser(in *user.FindUserRequest) (*user.FindUserResponse, error) {
-	// todo: add your logic here and delete this line
 
-	return &user.FindUserResponse{}, nil
+	var (
+		userEntitys []*models.Users
+		err         error
+	)
+	fmt.Println("phone : ", in.Phone)
+	if in.Phone != "" {
+		userEntity, err := l.svcCtx.UsersModel.FindByPhone(l.ctx, in.Phone)
+		if err == nil {
+			userEntitys = append(userEntitys, userEntity)
+		}
+	} else if in.Name != "" {
+		userEntitys, err = l.svcCtx.UsersModel.ListByName(l.ctx, in.Name)
+	} else if len(in.Ids) > 0 {
+		userEntitys, err = l.svcCtx.UsersModel.ListByIds(l.ctx, in.Ids)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	var resp []*user.UserEntity
+	copier.Copy(&resp, &userEntitys)
+
+	return &user.FindUserResponse{
+		User: resp,
+	}, nil
 }
--- apps/user/rpc/internal/logic/getuserinfologic.go
+++ apps/user/rpc/internal/logic/getuserinfologic.go
@@ -2,11 +2,18 @@ package logic
 
 import (
 	"context"
+	"easy-chat/apps/user/models"
+	"errors"
+	"github.com/jinzhu/copier"
 
 	"easy-chat/apps/user/rpc/internal/svc"
 	"easy-chat/apps/user/rpc/user"
 
 	"github.com/zeromicro/go-zero/core/logx"
+)
+
+var (
+	ErrUserNotFound = errors.New("没有找到用户")
 )
 
 type GetUserInfoLogic struct {
@@ -24,7 +31,19 @@ func NewGetUserInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetUs
 }
 
 func (l *GetUserInfoLogic) GetUserInfo(in *user.GetUserInfoRequest) (*user.GetUserInfoResponse, error) {
-	// todo: add your logic here and delete this line
 
-	return &user.GetUserInfoResponse{}, nil
+	userEntity, err := l.svcCtx.UsersModel.FindOne(l.ctx, in.Id)
+	if err != nil {
+		if !errors.Is(err, models.ErrNotFound) {
+			return nil, ErrUserNotFound
+		}
+		return nil, err
+	}
+
+	var resp user.UserEntity
+	copier.Copy(&resp, userEntity)
+
+	return &user.GetUserInfoResponse{
+		User: &resp,
+	}, nil
 }
